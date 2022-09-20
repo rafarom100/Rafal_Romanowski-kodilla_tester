36798433bfa831c26d864f77265b587453b5878a
Rafal Romanowski <rafarom@wp.pl>
2022-09-20 23:59:15 +0200

    Initial version

---
+++ src/main/java/User.java
@@ -0,0 +1,29 @@
+public class User {
+    public String name;
+    public int age;
+
+    public User (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        User piotr = new User("Piotr", 20);
+        User pawel = new User("Paweł", 30);
+        User marcin = new User("Marcin", 17);
+        User anna = new User("Anna", 40);
+        User paulina = new User("Paulina", 22);
+        User wiktor = new User("Wiktor", 68);
+        User donald = new User("Donald", 18);
+        User kunegunda = new User("Kunegunda", 53);
+
+        User[] users = {piotr, pawel, marcin, anna, paulina, wiktor, donald, kunegunda};
+
+        int sum = 0;
+        for (int i = 0)
+
+
+
+    }
+
+}
